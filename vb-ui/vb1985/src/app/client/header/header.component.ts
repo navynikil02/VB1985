b53b938399f7459544ac3ed7ecb58c9d4ec56da6
Nanjundeshwaran <navynikil02@gmail.com>
2023-07-14 01:20:52 +0530

    header commit

--- vb-ui/vb1985/src/app/client/header/header.component.ts
+++ vb-ui/vb1985/src/app/client/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { NavigationService } from 'src/app/services/navigation.service';
 
 @Component({
@@ -8,10 +9,40 @@ import { NavigationService } from 'src/app/services/navigation.service';
 })
 export class HeaderComponent {
 
-  constructor(private navigationService: NavigationService) { }
+  constructor(private navigationService: NavigationService, private router: Router) { }
 
   reloadPage(url: string): void {
     this.navigationService.navigateAndScroll(url);
   }
 
+  isFixedHeader = false;
+  isHomePage = false;
+  isHeaderActive = false;
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isHomePage = event.url === '/';
+        this.setIsFixedHeader();
+        this.setIsHeaderActive();
+      }
+    });
+
+    this.isHomePage = this.router.url === '/';
+    this.setIsFixedHeader();
+    this.setIsHeaderActive();
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    this.setIsHeaderActive();
+  }
+
+  private setIsFixedHeader() {
+    this.isFixedHeader = this.isHomePage;
+  }
+
+  private setIsHeaderActive() {
+    this.isHeaderActive = this.isHomePage && window.pageYOffset < 20;
+  }
 }
